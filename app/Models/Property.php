7bb81b70bc11039ebfef96f25f0c8eb375890546
daviezu <1david.hng@gmail.com>
2024-10-04 10:08:24 +0700

    add database and migration for property model

---
+++ app/Models/Property.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'property_name',
+        'address',
+        'latitude',
+        'longitude',
+        'price',
+        'description',
+        'bedroom',
+        'bathroom',
+        'area',
+        'property_type',
+        'status',
+        'image_path',
+        'facilites',
+    ];
+
+    public function PropertyToUser()
+    {
+        return $this->belongsTo(AppUser::class, 'user_id', 'user_id');
+    }
+}
