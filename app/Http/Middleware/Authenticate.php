9aa14fe5f341de235982e9fb96b6757ec2c2d995
daviezu <1david.hng@gmail.com>
2024-11-29 18:05:20 +0700

    add rememberMe middleware in authenticate

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\AppUser;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function checkRememberMe(Request $request, Closure $next): Response
+    {
+        if (!session()->has('user_id')) {
+            if (Cookie::has('remember_token')) {
+                $token = Cookie::get('remember_token');
+                $user = AppUser::where('remember_token', $token)->first();
+
+                if ($user) {
+                    session(['user_id' => $user->user_id]);
+                }
+            }
+        }
+
+        if (!session()->has('user_id')) {
+            return redirect('login')->with('error', 'Please log in to continue.');
+        }
+
+        return $next($request);
+    }
+}
