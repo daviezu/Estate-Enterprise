fa0f0a10639382a426773aac958fa86ec37a4f56
Kevin Hermanto <khermanto01@gmail.com>
2024-12-08 19:03:31 +0700

    fix: responsive navbar, footer, master

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,27 +1,26 @@
 <nav class="navbar navbar-expand pt-3" style="background-color: #ffffff; z-index: 1000;">
     <div class="container">
         <a class="navbar-brand nav-link active" aria-current="page" href="{{ route('home') }}">
-            <img src="{{ asset('images/EstateVerse Logo.png') }}" alt="Logo" width='200px'>
+            <img src="{{ asset('images/EstateVerse Logo.png') }}" class="logo" alt="Logo" width='200px'>
         </a>
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('property.list') }}">Properti Dijual</a>
             </li>
-            
             <li class="nav-item">
                 <a class="nav-link" href="#">Team Kami</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Hubungi Kami</a>
             </li>
-            <div class="search-container">
+            {{-- <div class="search-container">
                 <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <input class="form-control me-2 " type="search" placeholder="Search" aria-label="Search">
                     <button class="btn" type="submit">
                         <img src="{{ asset('images/Search Icon.png') }}" alt="search">
                     </button>
                 </form>
-            </div>
+            </div> --}}
 
             @if (session('is_logged_in', false))
                 <li class="nav-item">
@@ -43,10 +42,49 @@
 </nav>
 
 <style>
+    @media (max-width: 992px) {
+        .nav-item a,
+        .nav-item button{
+            font-size: 10px;
+        }
+
+        .search-container {
+            width: 250px !important;
+        }
+
+        .logo {
+            width: 100px;
+        }
+
+    }
+
+    @media (max-width: 768px) {
+        .logo {
+            width: 75px;
+        }
+
+    }
+
+    @media (max-width: 576px) {
+        .logo {
+            width: 70px;
+        }
+    }
+
+    @media (max-width: 480px) {
+
+    }
+    
+    .container {
+        width: 100%;
+    }
+
     .navbar {
+        width: 100%;
         border-bottom: solid;
         border-width: 1px;
         padding-bottom: 16px;
+        margin-bottom: 0px;
     }
 
     .search-container {
