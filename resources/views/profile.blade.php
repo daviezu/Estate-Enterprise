563fec241dd77e13373e200035fdcffe54593777
vnnico <nicolasbangka@gmail.com>
2024-12-10 16:03:48 +0700

    fix: profile page responsive

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -17,92 +17,89 @@
             </div>
         @endif
 
-        <div class="profile-section" style="margin-top: -10px; margin-left: 60px;">
+        <div class="container-fluid mt-5 pt-5">
 
-            <!-- Info Dasar -->
-            <i class="bi bi-info-circle"
-                style="font-size: 25px; color:#44D7B5; margin-left: 150px; position: relative; top: 27px; "></i>
-            <h5 class="fw-bold mb-4" style="font-size: 25px; font-family: Montserrat, sans-serif; color: #44D7B5;">Info
-                Dasar
-            </h5>
-            <div class="profile-card-info" style="margin-bottom: 70px;">
-                <div class="text-center my-3">
-                    <p class="mt-2 text" style="font-size: 16px; font-family: Montserrat, sans-serif;">Gambar Profile</p>
-                    {{-- <img src="{{ asset('images/profiles/rose.png') }}" alt="Profile Image" clgitass="profile-image"
-                        style="margin-bottom: 30px;"> --}}
-                    <div class="profile__picture">
-                        <img src="{{ asset($user->picture_path) }}" alt="Profile Image" clgitass="profile-image"
-                            style="margin-bottom: 30px;">
+            <div class="profile-section" >
+    
+                <!-- Info Dasar -->
+                <i class="bi bi-info-circle"
+                    style="font-size: 25px; color:#44D7B5; margin-left: 150px; position: relative; top: 27px; "></i>
+                <h5 class="fw-bold mb-4" style="font-size: 25px; font-family: Montserrat, sans-serif; color: #44D7B5;">Info
+                    Dasar
+                </h5>
+                <div class="profile-card-info d-flex flex-column" style="margin-bottom: 70px;">
+                    <div class="text-center my-3">
+                        <p class="mt-2 text" style="font-size: 16px; font-family: Montserrat, sans-serif;">Gambar Profile</p>
+                        {{-- <img src="{{ asset('images/profiles/rose.png') }}" alt="Profile Image" clgitass="profile-image"
+                            style="margin-bottom: 30px;"> --}}
+                        <div class="profile__picture">
+                            <img src="{{ asset($user->picture_path) }}" alt="Profile Image" clgitass="profile-image"
+                                style="margin-bottom: 30px;">
+                        </div>
                     </div>
+    
+                    <table class="table" style="margin-bottom: 50px;">
+    
+                        <tr style="border-bottom: 2px solid #dee2e6;">
+                            <th
+                                style="font-size: 16px; font-family: Montserrat, sans-serif; font-weight: 400; padding-bottom: 19px;">
+                                Nama Lengkap</th>
+                            <td style="font-size: 16px; font-family: Montserrat, sans-serif; ">
+                                {{ $user->fullname }}</td>
+                        </tr>
+    
+                        
+    
+                    </table>
+    
+                    <div class="button mt-auto ms-auto">
+                        <a href="{{ route('profile.edit.name.index') }}" class="btn btn-primary"
+                            style="font-weight: bold; color: #FFFFFF; background-color: #44D7B5; border: none; font-family: Montserrat, sans-serif; font-size: 16px;  width: 145px; height: 48px; border-radius: 10px; padding-top: 14px; margin-top: -5px;">Edit
+                            Profile</a>
+                    </div>
+    
                 </div>
-
-                <table class="table" style="margin-bottom: 50px;">
-
-                    <tr style="border-bottom: 2px solid #dee2e6;">
-                        <th
-                            style="font-size: 16px; font-family: Montserrat, sans-serif; font-weight: 400; padding-bottom: 19px;">
-                            Nama Lengkap</th>
-                        <td style="font-size: 16px; font-family: Montserrat, sans-serif; padding-right: 83px;">
-                            {{ $user->fullname }}</td>
-                    </tr>
-
-                    
-
-                </table>
-
-
-                
-
-                <div class="button">
-                    <a href="{{ route('profile.edit.name.index') }}" class="btn btn-primary"
-                        style="font-weight: bold; color: #FFFFFF; background-color: #44D7B5; border: none; font-family: Montserrat, sans-serif; font-size: 16px; float: right; margin-right: 50px; width: 145px; height: 48px; border-radius: 10px; padding-top: 14px; margin-top: -5px;">Edit
-                        Profile</a>
-                </div>
-
-            </div>
-
-
-            <!-- Privasi & Keamanan -->
-            <i class="bi bi-eye-slash"
-                style="font-size: 25px; color: #44D7B5; margin-left: 275px; position: relative; top: 28px;"></i>
-            <h5 class="fw-bold mb-4" style="font-size: 25px; font-family: Montserrat, sans-serif; color: #44D7B5;">Privasi &
-                Keamanan</h5>
-            <div class="profile-card-privacy" style="margin-bottom: 110px;">
-
-                <table class="table" style="margin-bottom: 50px;">
-
-                    <tr style="border-bottom: 2px solid #dee2e6;">
-                        <th
-                            style="font-size: 16px; font-family: Montserrat, sans-serif; font-weight: 400; padding-top: 25px; padding-bottom: 19px;">
-                            Email</th>
-                        <td
-                            style="font-size: 16px; font-family: Montserrat, sans-serif; padding-left: 375px; padding-top: 25px;">
-                            {{ $user->email }}</td>
-                    </tr>
-
-                    <tr style="border-bottom: 2px solid #dee2e6;">
-                        <th
-                            style="font-size: 16px; font-family: Montserrat, sans-serif; font-weight: 400; padding-top: 25px; padding-bottom: 19px;">
-                            Nomor Telepon</th>
-                        <td
-                            style="font-size: 16px; font-family: Montserrat, sans-serif; padding-left: 375px; padding-top: 25px;">
-                            {{ $user->phone_number }}</td>
-                    </tr>
-
-                    <tr style="border-bottom: 2px solid #dee2e6;">
-                        <th
-                            style="font-size: 16px; font-family: Montserrat, sans-serif; font-weight: 400; padding-bottom: 19px; padding-top: 21px;">
-                            Password</th>
-                        <td
-                            style="font-size: 16px; font-family: Montserrat, sans-serif; padding-top: 21px; padding-left: 375px;">
-                            ********</td>
-                    </tr>
-
-                </table>
-
-                <div class="button">
-                    <a href="{{ route('profile.edit.password.index') }}" class="btn btn-primary"
-                        style="font-weight: bold; color: #FFFFFF; background-color: #44D7B5; border: none; font-family: Montserrat, sans-serif; font-size: 16px; float: right; margin-right: 50px; width: 145px; height: 48px; border-radius: 10px; padding-top: 14px; margin-top: -22px;">Edit</a>
+    
+    
+                <!-- Privasi & Keamanan -->
+                <i class="bi bi-eye-slash"
+                    style="font-size: 25px; color: #44D7B5; margin-left: 275px; position: relative; top: 28px;"></i>
+                <h5 class="fw-bold mb-4" style="font-size: 25px; font-family: Montserrat, sans-serif; color: #44D7B5;">Privasi &
+                    Keamanan</h5>
+                <div class="profile-card-privacy d-flex flex-column" style="margin-bottom: 110px;">
+    
+                    <table class="table" style="margin-bottom: 50px;">
+    
+                        <tr style="border-bottom: 2px solid #dee2e6;">
+                            <th
+                                style="font-size: 16px; font-family: Montserrat, sans-serif; font-weight: 400; padding-bottom: 19px;">
+                                Email</th>
+                            <td style="font-size: 16px; font-family: Montserrat, sans-serif;">
+                                {{ $user->email }}</td>
+                        </tr>
+                        <tr style="border-bottom: 2px solid #dee2e6;">
+                            <th
+                                style="font-size: 16px; font-family: Montserrat, sans-serif; font-weight: 400; padding-bottom: 19px;">
+                                Nomor Telepon</th>
+                            <td style="font-size: 16px; font-family: Montserrat, sans-serif; ">
+                                {{ $user->phone_number }}</td>
+                        </tr>
+                        <tr style="border-bottom: 2px solid #dee2e6;">
+                            <th
+                                style="font-size: 16px; font-family: Montserrat, sans-serif; font-weight: 400; padding-bottom: 19px;">
+                                Password</th>
+                            <td style="font-size: 16px; font-family: Montserrat, sans-serif; ">
+                                ************</td>
+                        </tr>
+    
+                        
+    
+                    </table>
+    
+                    <div class="button ms-auto mt-auto">
+                        <a href="{{ route('profile.edit.password.index') }}" class="btn btn-primary ms-auto"
+                            style="font-weight: bold; color: #FFFFFF; background-color: #44D7B5; border: none; font-family: Montserrat, sans-serif; font-size: 16px;  width: 145px; height: 48px; border-radius: 10px; padding-top: 14px; margin-top: -22px;">Edit</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -151,8 +148,8 @@
         }
 
         .profile-card-info {
-            width: 1189px;
-            height: 569px;
+            width: 100%;
+            height: 420px;
             border: 1px solid #dee2e6;
             border-radius: 10px;
             padding: 20px;
@@ -160,7 +157,7 @@
         }
 
         .profile-card-privacy {
-            width: 1189px;
+            width: 100%;
             height: 320px;
             border: 1px solid #dee2e6;
             border-radius: 10px;
